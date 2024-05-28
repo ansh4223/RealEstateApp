5014b7c83bf308f71b527ee4ea4fb368a494d4c9
ansh4223 <ak210073@gmail.com>
2024-05-28 23:39:35 +0530

    setting up and structuring auth controller

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import authRoute from "./routes/post.route.js";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
 
 app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(5500, () => {
     console.log("Server is running");
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,17 +1,12 @@
 import express from "express"
+import { login, logout, register } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
-router.post("/register", (req,res) => {
-    console.log("route works");
-});
+router.post("/register", register);
 
-router.post("/login", (req,res) => {
-    console.log("route works");
-});
+router.post("/login", login);
 
-router.post("/logout", (req,res) => {
-    console.log("route works");
-});
+router.post("/logout", logout);
 
 export default router;

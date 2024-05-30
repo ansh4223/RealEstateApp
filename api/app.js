cf8705199ea5ada464c2bd0beed798a3f2910cdd
ansh4223 <ak210073@gmail.com>
2024-05-30 22:04:01 +0530

    fixing server

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ import authRoute from "./routes/post.route.js";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
-
+app.use(express.json());
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
-app.listen(5500, () => {
+app.listen(8800, () => {
     console.log("Server is running");
 })
